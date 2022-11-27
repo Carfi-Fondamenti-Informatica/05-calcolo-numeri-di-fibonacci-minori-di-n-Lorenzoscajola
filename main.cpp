e85286b710044a1970c65129f77292594a12ace6
Lorenzoscajola <117931214+Lorenzoscajola@users.noreply.github.com>
2022-11-27 17:36:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-using namespace std;
+using namespace std; 
 
 int main() {
+	int n=0;
+	int a=1;
+	int b=1;
+	int c=0;
+	cin>> n; 
+	
+	cout<<a<<endl; 
+	cout<<b<<endl; 
+	for(int i=0; i<=n; i++){
+	   c=a+b;
+	   a=b;
+	   b=c;
+	   cout<<c<<endl;}
    return 0;
 }
